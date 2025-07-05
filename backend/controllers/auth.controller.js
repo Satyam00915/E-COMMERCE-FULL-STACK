1da7c3fc1cd2c1093c95dcf65b393f2f347ba0c9
Satyam00915 <satyamupadhyay0495@gmail.com>
2025-07-06 00:56:52 +0530

    MongoDB_Connected

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  res.json({
+    message: "Sign Up Route",
+  });
+};
+
+export const signin = async (req, res) => {
+  res.json({
+    message: "Sign In Route",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    message: "Log Out Route",
+  });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRouter from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
