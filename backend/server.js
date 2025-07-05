a5cb5698eedf25c7073a48192acbf05923b940d6
Satyam00915 <satyamupadhyay0495@gmail.com>
2025-07-06 00:29:50 +0530

    Basic_Backend_Setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT;
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
