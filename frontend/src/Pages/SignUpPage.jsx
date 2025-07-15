8d184cd85a65c0a00b1f0cf001f1c5d48f461333
Satyam00915 <satyamupadhyay0495@gmail.com>
2025-07-15 23:23:19 +0530

    SignUp_Page_Started

--- frontend/src/Pages/SignUpPage.jsx
+++ frontend/src/Pages/SignUpPage.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 export const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const loading = true;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <motion.div
+        className="sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-emerald-400">
+          Create Your account
+        </h2>
+      </motion.div>
+    </div>
+  );
 };
